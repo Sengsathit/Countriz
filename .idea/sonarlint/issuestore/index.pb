e402872838eabdaa3a847de1db1b4d05e514ee21
Sengsathit <sengsathit@gmail.com>
2022-10-15 00:09:16 +0200

    Partie 2

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,5 @@
 
 j
 :app/src/main/java/com/mycompany/countriz/ui/theme/Theme.kt,0/b/0b3380f65b025b0852177a091e51c0af72e5b276
-q
-Aapp/src/main/java/com/mycompany/countriz/ui/screens/MainScreen.kt,b/0/b0a489fe2a2fb353863f1974a931c956f42a7391
-P
- app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
+j
+:app/src/main/java/com/mycompany/countriz/ui/theme/Color.kt,c/1/c1e377a15fa04f8a0a5c31e356979b71a13b7ca3
--- app/src/main/java/com/mycompany/countriz/MainActivity.kt
+++ app/src/main/java/com/mycompany/countriz/MainActivity.kt
@@ -3,17 +3,16 @@ package com.mycompany.countriz
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.mycompany.countriz.ui.screens.MainScreen
+import com.mycompany.countriz.ui.screens.CountriesListScreen
 import com.mycompany.countriz.ui.theme.CountrizTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             CountrizTheme {
-                MainScreen()
+                CountriesListScreen()
             }
         }
     }
